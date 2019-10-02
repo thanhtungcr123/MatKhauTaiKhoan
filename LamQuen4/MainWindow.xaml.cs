5f6c147abc847cbef84633abe72cfd806155251b
thanhtungcr123 <01694340878cr@gmail.com>
2019-10-02 13:30:36 +0700

    Add project files.

---
+++ LamQuen4/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LamQuen4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Co_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ten : " + Ten_dang_nhap.Text + "\nMat khau : " + Mat_khau.Text);
+        }
+
+        private void Khong_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
